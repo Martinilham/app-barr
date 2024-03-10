c4b7435e36644ad26727f16b4d8296700ef18c10
Martin <ilhammartin2@gmail.com>
2024-03-10 21:44:46 +0700

    app-bar

--- appbar/lib/main.dart
+++ appbar/lib/main.dart
@@ -1,25 +1,48 @@
 import "package:flutter/material.dart";
 
 void main(){
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
+    title: "Aplikasi Degan Shop",
     home: HalamanUtama()
   ));
 }
 
 class HalamanUtama extends StatelessWidget{
+  const HalamanUtama({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 117, 193, 255),
       appBar: AppBar(
-        backgroundColor: const Color.blue,
+        backgroundColor: const Color.fromARGB(255, 7, 144, 255),
         leading: const Icon(Icons.home),
-        title: const Text("Degan Store"),
+        title: const Center(
+          child: Text("Degan Store")),
         actions: const <Widget>[
           Icon(Icons.search)
         ],
       ),
-      body:,
+      body: Container(
+        margin: const EdgeInsets.only(
+          top: 4.0,
+          left: 2.0,
+          right: 2.0
+        ),
+        width: 280.0,
+        height: 100.0,
+        color: const Color.fromARGB(255, 0, 55, 206),
+        child: 
+        const Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.home),
+              Text("Degan Ijo")
+            ],
+          ),
+        )
+      )
     );
   }
 }
